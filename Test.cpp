470d9a1ba0a3dd66acb7f11c26d32d63e89f3a34
--unset <dennis.g1993@gmail.com>
2019-05-03 19:11:28 +0300

    added my own tests

--- Test.cpp
+++ Test.cpp
@@ -1,6 +1,6 @@
 /**
  * A demo program for bull-pgia.
- * 
+ *
  * @author Erel Segal-Halevi
  * @since  2019-04
  */
@@ -36,7 +36,7 @@ int main() {
 
 		testcase.setname("Play with dummy choosers and guessers")
 		.CHECK_EQUAL(play(c1234, g1234, 4, 100), 1)      // guesser wins in one turn.
-		.CHECK_EQUAL(play(c1234, g9999, 4, 100), 101)    // guesser loses by running out of turns 
+		.CHECK_EQUAL(play(c1234, g9999, 4, 100), 101)    // guesser loses by running out of turns
 		.CHECK_EQUAL(play(c1234, g12345, 4, 100), 101)   // guesser loses technically by making an illegal guess (too long).
 		.CHECK_EQUAL(play(c12345, g1234, 4, 100), 0)     // chooser loses technically by choosing an illegal number (too long).
 		;
@@ -48,6 +48,21 @@ int main() {
 			testcase.CHECK_EQUAL(play(randy, smarty, 4, 100)<=100, true);  // smarty should always win in at most 10 turns!
 		}
 
+		/*
+		/////////////////////////////////My Tests///////////////////////////////
+		*/
+		.CHECK_OUTPUT(calculateBullAndPgia("78945","78945"), "5,0")      // 5 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("178964","719846"), "0,6")      // 0 bull, 6 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("123","456"), "0,0")      // 0 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("741","418"), "0,2")      // 0 bull, 2 pgia
+
+		testcase.setname("My Test with smarty") ;
+
+		for(uint i=0 < 100 ; i++){
+			int length = rand()%4 + 1 ;
+			testcase.CHECK_EQUAL(play(randy, smarty, length , 100) <100 , true);
+		}
+
     grade = testcase.grade();
 	} else {
 		testcase.print_signal(signal);
@@ -56,4 +71,3 @@ int main() {
 	cout << "Your grade is: "  << grade << endl;
 	return 0;
 }
-
